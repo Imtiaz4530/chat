695d60c91583fa8e21cd7deee0bd2506b13c0c4a
Imtiaz4530 <smimtiaz58@gmail.com>
2025-02-04 20:24:29 +0600

    some fix in server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-import path from "path";
 
 import { app, server } from "./socket/socket.js";
 import connectDB from "./db/mongodb.js";
@@ -10,16 +9,6 @@ import connectCloudinary from "./utils/cloudinary.js";
 import authRoute from "./routes/auth.routes.js";
 import messageRoute from "./routes/message.routes.js";
 import userRoute from "./routes/user.routes.js";
-
-const __dirname = path.resolve();
-app.use("/uploads/files", (req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
-
-app.use("/uploads/files", express.static("uploads/files"));
 
 app.use(express.json());
 app.use(
@@ -36,11 +25,6 @@ await connectCloudinary();
 app.use("/api/auth", authRoute);
 app.use("/api/chats", messageRoute);
 app.use("/api/users", userRoute);
-
-app.use(express.static(path.join(__dirname, "/client/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
 
 app.get("/", (req, res) => {
   res.send("API WORKING !!!");
