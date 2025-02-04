433f4e24d2d139efd86ac70f641ee4b5f3fd4a89
Imtiaz4530 <smimtiaz58@gmail.com>
2025-02-04 21:10:51 +0600

    naming fix auth context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import { StoreProvider } from "easy-peasy";
 
 import "./index.css";
 import App from "./App.jsx";
-import AuthContextProvider from "./context/authContext.jsx";
+import AuthContextProvider from "./context/AuthContext.jsx";
 import store from "./store/store.js";
 
 createRoot(document.getElementById("root")).render(
